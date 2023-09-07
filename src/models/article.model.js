defb569859b0082bfd251eabc2979d90ab705c71
Pacome KFP <pacomekengafe@gmail.com>
2023-09-07 19:16:29 +0100

    Tous les models sont créés

---
+++ src/models/article.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  landing: {
+    type: String,
+    default: "/public/images/topics/environment.png",
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Users",
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Comments",
+    },
+  ],
+  topics: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Topics",
+    },
+  ],
+});
+
+const ArticleModel = new mongoose.model("Articles", articleSchema);
+
+module.exports = { ArticleModel };
---
+++ src/models/topic.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const topicSchema = new mongoose.Schema({
+  label: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  articles: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Articles",
+    },
+  ],
+  landing: {
+    type: String,
+    default: "/public/images/topics/environment.png",
+  },
+});
+
+const TopicModel = new mongoose.model("Topics", topicSchema);
+
+module.exports = { TopicModel };
